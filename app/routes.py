3bee46b7f350cb2ddc3dec07c949243aa3b43979
Gipfeli <nguyenchanhdat@gmail.com>
2018-12-25 05:37:31 +0100

    Update templates

--- app/routes.py
+++ app/routes.py
@@ -11,4 +11,13 @@ from app import app
 @app.route('/index')
 
 def index():
-	return "Hello World"
+	user = {'username': 'Gipfeli'}
+	return '''
+<html>
+	<head>
+		<title>Home Page - Microblog</title>
+	</head>
+	<body>
+		<h1>Hello ''' +user['username'] + '''!</h1>
+	</body>
+</html>'''
