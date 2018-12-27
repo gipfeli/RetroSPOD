32a2735ea42789ab141a71bc38be570188084f7a
Gipfeli <nguyenchanhdat@gmail.com>
2018-12-28 00:50:51 +0100

    Setting login page up

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,17 @@ Created on Tue Dec 25 02:56:40 2018
 """
 
 from flask import Flask
+from flask_login import LoginManager
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
