cc77a4e16d08d2f10dded0bc31c2ad4ebf88f753
Gipfeli <nguyenchanhdat@gmail.com>
2018-12-28 14:41:54 +0100

    Add Gravatar support

--- app/routes.py
+++ app/routes.py
@@ -5,19 +5,21 @@ Created on Tue Dec 25 03:39:52 2018
 @author: Gipfeli
 """
 
-from flask import render_template, flash, redirect, url_for
-from flask import request
-from flask_login import current_user, login_user
-from flask_login import logout_user
-from flask_login import login_required
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 
-from app import app
-from app.forms import LoginForm
-from app.forms import RegistrationForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
-from app import db
 
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -75,6 +77,32 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
