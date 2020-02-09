04475b722f5eb85ab1a53c8a433e014403899f85
Dat Nguyen <nguyenchanhdat@gmail.com>
2020-02-09 22:37:15 +0100

    Mockup sites
    Index site.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
-    
+    # Display content of Index site
+    ### return "Hello World"
+    user = {'username': 'gipfeli'}
+    posts = [
+    	{
+    		'author': {'username': 'John'},
+    		'body': 'Windy night in Uzwil'
+    	},
+    	{
+    		'author': {'username': 'Wick'},
+    		'body': 'Fortune favors the bold!'
+    	}		
+    ]
+    return render_template('index.html', 
+    						title = 'Home', 
+    						user=user,
+    						posts=posts)
