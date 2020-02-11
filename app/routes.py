7b06b421311b58f1db15f949ced52f38ac7537f5
Gipfeli <gipfeli@W530.localdomain>
2020-02-11 22:07:05 +0100

    Add login forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
+from flask import flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -21,3 +23,13 @@ def index():
     						title = 'Home', 
     						user=user,
     						posts=posts)
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user{}, remember_me={}'.format(
+			form.username.data, form.remember_me.data
+		))
+		return redirect('/index')
+    return render_template('login.html', title = 'Sign In', form=form)    
